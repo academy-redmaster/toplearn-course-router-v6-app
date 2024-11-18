42c16935653fb400a89e32b6ecfdfffa8991eef4
Ali Shahbaz <147451904+academy-redmaster@users.noreply.github.com>
2024-11-18 15:57:50 +0330

    add browser router in react router dom project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Link, Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center">
-      <h1 className="text-2xl">Hello world</h1>
+    <div className="min-h-screen w-full flex flex-col items-center justify-center text-5xl">
+      <h1>welcome to react router course</h1>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import "remixicon/fonts/remixicon.css";
 import { NextUIProvider } from "@nextui-org/react";
-import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <NextUIProvider>
-    <NextThemesProvider attribute="class" defaultTheme="dark">
-      <App />
-    </NextThemesProvider>
-  </NextUIProvider>
+  <BrowserRouter>
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <App />
+      </NextThemesProvider>
+    </NextUIProvider>
+  </BrowserRouter>
 );
