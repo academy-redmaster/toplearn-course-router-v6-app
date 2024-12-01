46ff40d52809714c5fac6a5237f68e65f8475dc3
Ali Shahbaz <147451904+academy-redmaster@users.noreply.github.com>
2024-12-01 17:10:53 +0330

    feat: complete TodoLayout design and implement Todo list rendering

--- src/pages/todoIndex.jsx
+++ src/pages/todoIndex.jsx
@@ -1,8 +1,57 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useAuth } from "../hook/useAuth";
+import { Chip } from "@nextui-org/chip";
+import CustomLoader from "../components/customLoader";
+import TableTodo from "../components/tableTodo";
+
+const columns = [
+  { name: "TITLE", uid: "title" },
+  { name: "DUEDATE", uid: "dueDate" },
+  { name: "STATUS", uid: "status" },
+  { name: "ACTIONS", uid: "actions" },
+];
+
 export default function TodoIndexPage() {
-    return (
-      <div >
-        <h1>TodoIndexPage</h1>
+  const { userId } = useAuth();
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const location = useLocation();
+
+  // get todos
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const queryParams = location.search;
+        const response = await axios.get(
+          `http://localhost:8008/api/todos/${queryParams}`
+        );
+        setTodos(response.data);
+      } catch (error) {
+        console.log("🚀 ~ error:", error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [location.search]);
+  console.log("🚀 ~ TodoIndexPage ~ todos:", todos);
+
+  return (
+    <div className="space-y-6 py-10">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-semibold text-secondary">
+          Todo List: <span className="text-sm">{userId}</span>
+        </h1>
+        <Chip color="danger" variant="shadow" size="lg" className="text-xl">
+          {todos.length}
+        </Chip>
       </div>
-    );
-  }
-  
+      {loading ? <CustomLoader /> : <TableTodo columns={columns} todos={todos} />}
+    </div>
+  );
+}
