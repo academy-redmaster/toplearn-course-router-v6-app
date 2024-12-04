b4bb5d90508870dc1895ce049fe9ac028aa2dc20
Ali Shahbaz <147451904+academy-redmaster@users.noreply.github.com>
2024-12-04 10:25:09 +0330

    feat: complete TodoComplete page with task completion functionality

--- src/pages/todoArchive.jsx
+++ src/pages/todoArchive.jsx
@@ -1,8 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import CustomLoader from "../components/customLoader";
+
 export default function TodoArchivePage() {
-    return (
-      <div >
-        <h1>TodoArchivePage</h1>
-      </div>
-    );
-  }
-  
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // change todo data
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const check = await axios.get(`http://localhost:8008/api/todos/${id}`);
+        console.log("🚀 ~ check:", check);
+        if (check.data && check.data.isArchived !== undefined) {
+          if (check.data.isArchived) {
+            const response = await axios.patch(
+              `http://localhost:8008/api/todos/${id}/unarchive`
+            );
+            if (response.status === 200 || response.status === 204) {
+              toast.warning("task updated successfully UnArchived ");
+              navigate("..", { replace: true });
+            }
+          } else {
+            const response = await axios.patch(
+              `http://localhost:8008/api/todos/${id}/archive`
+            );
+            if (response.status === 200 || response.status === 204) {
+              toast.success("task updated successfully Archived ");
+              navigate("..", { replace: true });
+            }
+          }
+        } else {
+          throw new Error("Invalid response from the server");
+        }
+      } catch (error) {
+        console.log("🚀 ~ error:", error);
+        toast.error(`~ error archive task : ${error.message}`);
+        navigate("/todo", { replace: true });
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [id]);
+  if (loading) return <CustomLoader />;
+  return;
+}
--- src/pages/todoComplete.jsx
+++ src/pages/todoComplete.jsx
@@ -1,13 +1,45 @@
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import CustomLoader from "../components/customLoader";
 
 export default function TodoCompletePage() {
-  const {id , complete} = useParams()
-    console.log("🚀 ~ TodoCompletePage ~ params id:", id)
-    console.log("🚀 ~ TodoCompletePage ~ params complete:", complete)
-    return (
-      <div >
-        <h1>TodoCompletePage</h1>
-      </div>
-    );
-  }
-  
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // change data
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const check = await axios.get(`http://localhost:8008/api/todos/${id}`);
+        if (check.data && check.data.isCompleted !== undefined) {
+          if (check.data.isCompleted) {
+            toast.info("the task is already completed, no change made.");
+            navigate("..", { replace: true });
+          } else {
+            const response = await axios.patch(
+              `http://localhost:8008/api/todos/${id}/complete`
+            );
+            if (response.status === 200 || response.status === 204) {
+              toast.success("task updated successfully");
+              navigate("..", { replace: true });
+            }
+          }
+        } else {
+          throw new Error("Invalid response from the server");
+        }
+        console.log("🚀 ~ check:", check);
+      } catch (error) {
+        toast.error(`~ error complete task: ${error.message}`);
+        navigate("/todo", { replace: true });
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [id]);
+  if (loading) return <CustomLoader />;
+  return;
+}
